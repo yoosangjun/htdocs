ba1b4ec8e8fc2126de17502562b84332c8eb0f6d
yoo sang jun <52834205+yoosangjun@users.noreply.github.com>
2022-11-18 16:39:49 +0900

    first

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>게시판실습</title>
+    <link rel="stylesheet" href="./style.css" />
+  </head>
+  <body>
+    <header>
+      <nav>
+        <h1>먹튀인벤</h1>
+        <ul>
+          <li>보증사이트</li>
+          <li>자유게시판</li>
+          <li>먹튀사이트</li>
+          <li>고객센터</li>
+        </ul>
+      </nav>
+    </header>
+    <main class="main__wrap">
+      <section class="board">
+        <div class="board__title">
+          <h2>공지사항</h2>
+          <p>공지사항을 빠르고 정확하게 안내해 드립니다.</p>
+        </div>
+        <div class="board__list__wrap">
+          <div class="top">
+            <div class="top__number">번호</div>
+            <div class="top__title">제목</div>
+            <div class="top__writer">글쓴이</div>
+            <div class="top__date">작성일</div>
+            <div class="top__count">조회수</div>
+          </div>
+        </div>
+      </section>
+    </main>
+  </body>
+</html>
